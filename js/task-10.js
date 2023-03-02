b9ab678e037127bd23e66328b8c64f534b87432b
YuraBuchak <115108617+YuraBuchak@users.noreply.github.com>
2023-03-02 15:14:18 +0200

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,28 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnCreateEl = document.querySelector("button[data-create]");
+const btnDestroyEl = document.querySelector("button[data-destroy]");
+const inputEl = document.querySelector("input");
+const boxEl = document.querySelector("#boxes");
+
+const createMarkup = function () {
+  let amount = inputEl.value;
+  const firstSizeEl = 30;
+
+  for (let i = 0; i < amount; i += 1) {
+    let randomBgrColor = getRandomHexColor();
+    let divSizeEl = i * 10 + firstSizeEl;
+    const divEl = `<div width="${divSizeEl}px" heigth="${divSizeEl}px" background-color ="${randomBgrColor}"></div>`;
+
+    divSizeEl = boxEl.insertAdjacentHTML("afterbegin", divEl);
+  }
+  // console.log(boxEl);
+};
+
+const handleCreateBtn = btnCreateEl.addEventListener("click", createMarkup);
+const handleDestroyBtn = btnDestroyEl.addEventListener(
+  "click",
+  () => (boxEl.innerHTML = "")
+);
